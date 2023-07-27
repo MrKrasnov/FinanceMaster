f21146f7c02ef3a2ddd1d9c1a19d85cfa45eb361
mrkrasnov <mr.krasnov.y@gmail.com>
2023-07-27 23:35:46 +0300

    added square brackets for logs

--- app/Core/Log.php
+++ app/Core/Log.php
@@ -6,8 +6,8 @@ class Log
 {
     public static function writeLog(string $text): string
     {
-        $file = fopen(ABSOLUTE_LOG_PATH . DIRECTORY_SEPARATOR .date('Y-m-d_H-s').'.log', 'ab');
-        $data = PHP_EOL . date('Y.m.d H:s') . " $text " . PHP_EOL;
+        $file = fopen(ABSOLUTE_LOG_PATH . DIRECTORY_SEPARATOR .date('Y-m-d').'.log', 'ab');
+        $data = '['. date('Y.m.d H:s') . "] $text " . PHP_EOL;
         fwrite($file, $data);
 
         return $data;
