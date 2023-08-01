a57cafdfba361fb06ceef381700894d7800a22f1
mrkrasnov <mr.krasnov.y@gmail.com>
2023-08-01 21:50:15 +0300

    Added comments of PHPdoc for variables in the "default.php" HTML template, as ide phpstorm was writing warnings for variables

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -2,13 +2,21 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-	<title><?php echo $title; ?></title>
+    <title>
+        <?php
+        /** @var string $title */
+        echo $title;
+        ?>
+    </title>
     <link rel="stylesheet" href="./public/css/reset.css">
     <link rel="stylesheet" href="./public/css/style.css">
     <link href="./public/img/favicon.ico" rel="icon" type="image/x-icon" />
     <script src="./vendor/apexcharts/apexcharts.js"></script>
 <body>
-<?php echo $content; ?>
+<?php
+/** @var string $content */
+echo $content;
+?>
 <footer>
     <div class="logo-fm"></div>
     Copyright (c) <?php echo date('Y') ?> MrKrasnov/FinanceMaster
