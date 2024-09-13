672fa5df92d1c3c4e9dccb75badcff60f4515f7c
asinastretche <yochikaffiel@gmail.com>
2024-09-13 16:17:29 +0300

    редактирование генерации csrf-токена

--- app/Models/Authentication.php
+++ app/Models/Authentication.php
@@ -12,13 +12,10 @@ class Authentication extends Model
     {
         parent::__construct();
         $this->generateCSRFToken();
-        $this->csrfToken = $_SESSION['csrf_token'];
     }
 
     private function generateCSRFToken()
     {
-//        Нужно создать генерацию csrf токена, затем он должен записываться в свойство класса app/Models/Authentication.php
-//        Далее это свойство нужно записать в глобальный массив $_SESSION['csrf_token']
         if (!isset($_SESSION['csrf_token'])){
             $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
         }
