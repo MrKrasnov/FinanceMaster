2c771c421f0a0795e370da51f7716543cabf9c0d
MrKrasnov <mr.krasnov.y@gmail.com>
2024-09-14 03:18:51 +0300

    added expire date for csrf token

--- app/Models/Authentication.php
+++ app/Models/Authentication.php
@@ -7,19 +7,31 @@ use App\Core\Model;
 class Authentication extends Model
 {
     public string $csrfToken;
+    public string $csrfTokenExpire;
 
     public function __construct()
     {
         parent::__construct();
 
         $this->generateCSRFToken();
+        $this->generateCSRFTokenLifeExpire();
     }
 
-    private function generateCSRFToken()
+    private function generateCSRFToken(): void
     {
         if (!isset($_SESSION['csrf_token'])){
             $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
         }
+
         $this->csrfToken = $_SESSION['csrf_token'];
+    }
+
+    private function generateCSRFTokenLifeExpire(): void
+    {
+        if (!isset($_SESSION['csrf_token_expire'])){
+            $_SESSION['csrf_token_expire'] = time() + 3600;
+        }
+
+        $this->csrfTokenExpire = $_SESSION['csrf_token_expire'];
     }
 }
