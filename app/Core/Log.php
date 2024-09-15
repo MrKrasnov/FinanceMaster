376ebdc2ffbcb2a8dc1e62cdf8ce9d2689ca6e30
MrKrasnov <mr.krasnov.y@gmail.com>
2024-09-15 14:11:16 +0300

    Specified a relative path for logs - part 2

--- app/Core/Log.php
+++ app/Core/Log.php
@@ -6,7 +6,7 @@ class Log
 {
     public static function writeLog(string $text): string
     {
-        $file = fopen(ABSOLUTE_LOG_PATH . DIRECTORY_SEPARATOR .date('Y-m-d').'.log', 'ab');
+        $file = fopen(RELATIVE_LOG_PATH . DIRECTORY_SEPARATOR .date('Y-m-d').'.log', 'ab');
         $data = '['. date('Y.m.d H:s') . "] $text " . PHP_EOL;
         fwrite($file, $data);
 
