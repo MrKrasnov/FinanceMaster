e27cd85ac46559a119210f2d357b7f15d39825d8
MrKrasnov <mr.krasnov.y@gmail.com>
2025-03-09 13:33:07 +0200

    Added a check-stopper for log recording

--- app/Core/Log.php
+++ app/Core/Log.php
@@ -6,7 +6,12 @@ class Log
 {
     public static function writeLog(string $text): string
     {
-        $file = fopen(RELATIVE_LOG_PATH . DIRECTORY_SEPARATOR .date('Y-m-d').'.log', 'ab');
+        $file = fopen(RELATIVE_LOG_PATH . date('Y-m-d').'.log', 'ab');
+        
+        if($file === false) {
+            return 'Error write log. ' . 'Error Message: ' . $text;
+        }
+
         $data = '['. date('Y.m.d H:s') . "] $text " . PHP_EOL;
         fwrite($file, $data);
 
