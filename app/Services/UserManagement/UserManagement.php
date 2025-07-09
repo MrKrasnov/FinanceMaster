f19bacb4077132090b1abc95c5fe6f08199b526d
MrKrasnov <mr.krasnov.y@gmail.com>
2025-07-09 23:01:18 +0300

    added save user in db

--- app/Services/UserManagement/UserManagement.php
+++ app/Services/UserManagement/UserManagement.php
@@ -2,10 +2,49 @@
 
 namespace App\Services\UserManagement;
 
+use App\Core\DB;
+use App\Services\SQLQueryBuilder\InsertQueryBuilder;
+use App\Services\SQLQueryBuilder\SelectQueryBuilder;
+use PDO;
+
 class UserManagement
 {
+    private PDO $pdoDB;
+    
+    public function __construct()
+    {
+        $db = new DB();
+        $this->pdoDB = $db->db;
+    }
+
     public function registration(string $login, string $email, string $password)
     {
-        //TODO: set user in the database.
+        if($this->checkUserByEmail($email)) {
+            return;
+        }
+
+        $sqlstring = new InsertQueryBuilder();
+        $sqlstring
+            ->insertInto('users')
+            ->setValues([
+                'login' => $login,
+                'email' => $email,
+                'password' => password_hash($password, PASSWORD_DEFAULT)
+            ])->execute($this->pdoDB);
+
+
+        
+    }
+
+    private function checkUserByEmail($email) : Bool
+    {
+        $selectSql = new SelectQueryBuilder();
+        $selectSql
+            ->select(['email'])
+            ->from('users')
+            ->where(['email'=> $email]);
+
+        $result = $selectSql->execute($this->pdoDB);
+        return empty($result);
     }
 }
