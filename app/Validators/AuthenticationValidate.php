30b50d3037c6dbfb4cecb1bf3d5e1d7e46a69728
MrKrasnov <mr.krasnov.y@gmail.com>
2025-07-11 16:53:28 +0300

    added validation condition for sign in

--- app/Validators/AuthenticationValidate.php
+++ app/Validators/AuthenticationValidate.php
@@ -2,13 +2,36 @@
 
 namespace App\Validators;
 
+use App\Core\Manager\CsrfTokenManager;
 use App\Core\Validate;
+use App\Exceptions\ValidationException;
 
 class AuthenticationValidate extends Validate
 {
+    /**
+     * @throws ValidationException
+     */
     public function validate() : bool
     {
         if(!$this->isPOSTrequest()) {
+            return false;
+        }
+
+        if(!$this->isFormDataContentType()) {
+            return false;
+        }
+
+        if(!$this->isJsonAcceptType()) {
+            return false;
+        }
+
+        if(!CsrfTokenManager::validateCSRFToken()) {
+            return false;
+        }
+
+        $requiredFields = ['username', 'password'];
+
+        if (!$this->isNotEmptyPostFields($requiredFields)) {
             return false;
         }
 
