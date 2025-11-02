2079f3883e4e922db3d5fe04676f1d3785615f1e
MrKrasnov <mr.krasnov.y@gmail.com>
2025-11-02 04:42:34 +0200

    Fixed error when write logs

--- app/config/const.php
+++ app/config/const.php
@@ -1,4 +1,11 @@
 <?php
 const RELATIVE_LOG_PATH = __DIR__ . "/../../../logs/";
+
+if (!is_dir(RELATIVE_LOG_PATH)) {
+    if (!mkdir(RELATIVE_LOG_PATH, 0755, true)) {
+        error_log('Error: Failed to create log directory: ' . RELATIVE_LOG_PATH);
+    }
+}
+
 define("REQUEST_URI", $_SERVER['REQUEST_URI']);
 define("USER_AGENT", $_SERVER['HTTP_USER_AGENT']);
